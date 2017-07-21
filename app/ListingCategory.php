e98ba817339e1196c21d79732552d8b10e985ee9
Shashank Saudagar <shashank@ajency.in>
2017-07-21 16:20:00 +0530

    changed

--- app/ListingCategory.php
+++ app/ListingCategory.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class ListingCategory extends Model
 {
     protected $table = "listing_category";
+    // protected $primaryKey = ['listing_id', 'category_id'];
+    protected $fillable=['listing_id','category_id'];
 }
